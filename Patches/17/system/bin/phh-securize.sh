b11ffeea8501245c9eade6f9932b45a7c013a31e
rofikkerneldev <rofikoga@gmail.com>
2026-05-13 12:38:56 +0200

    add 17 missing files

---
+++ Patches/17/system/bin/phh-securize.sh
@@ -0,0 +1,18 @@
+#!/system/bin/sh
+
+# Remove bind-mount of phh-su
+mount -o rw,remount /
+umount -l /system/xbin/su
+umount -l /system/bin/magisk
+
+# Nuke
+rm /system/xbin/su
+rm /system/bin/phh-su
+rm /system/etc/init/su.rc
+rm /system/bin/phh-root.sh
+rm -rf /system/priv-app/SuperUser
+rm /system/bin/phh-securize.sh
+rm -rf /data/su || true
+mount -o ro,remount /
+sync
+reboot
